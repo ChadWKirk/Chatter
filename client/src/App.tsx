394dd038de998e8c2e9cbf687630f8c78a8763d7
Chad Kirk <ChadWKirk@gmail.com>
2023-10-03 11:35:11 -0500

    Styled enter page for desktop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./Normalize.css";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div id="enterContainer">
+      <rect id="rect1"></rect>
+      <rect id="rect2"></rect>
+      <circle></circle>
+      <h1 id="enterH1">Chatter</h1>
+      <form id="enterForm">
+        <input id="enterInput" placeholder="Name"></input>
+        <a id="enterButton" href="#">
+          Enter Room
+        </a>
+      </form>
+    </div>
+  );
 }
 
 export default App;
